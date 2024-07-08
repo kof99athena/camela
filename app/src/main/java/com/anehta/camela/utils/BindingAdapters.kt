83d54dd21ccaa05421ddc796b11b3a6b26ca13ef
kof99athena <kof99athena@gmail.com>
2024-07-08 22:12:24 +0900

    Refactor: Update Ratio UI
    Change TextView to ImageView

--- app/src/main/java/com/anehta/camela/utils/BindingAdapters.kt
+++ app/src/main/java/com/anehta/camela/utils/BindingAdapters.kt
@@ -1,19 +1,24 @@
 package com.anehta.camela.utils
 
+import android.media.Image
 import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.anehta.camela.R
 
 @BindingAdapter("app:ratioToText")
-fun setRatioToText(view: TextView, ratio: ScreenUtil.Ratio?) {
+fun setRatioToText(view: ImageView, ratio: ScreenUtil.Ratio?) {
     ratio?.let {
         Log.d("BindingAdapter", "Ratio: $ratio")
-        view.text = when (it) {
-            ScreenUtil.Ratio.Ratio_1_1 -> "1:1"
-            ScreenUtil.Ratio.Ratio_3_4 -> "3:4"
-            ScreenUtil.Ratio.Ratio_9_16 -> "9:16"
-            ScreenUtil.Ratio.Ratio_Full -> "Full"
-        }
+        view.setImageResource(
+            when (it) {
+                ScreenUtil.Ratio.Ratio_1_1 -> R.drawable.ic_11
+                ScreenUtil.Ratio.Ratio_3_4 -> R.drawable.ic_34
+                ScreenUtil.Ratio.Ratio_9_16 -> R.drawable.ic_916
+                ScreenUtil.Ratio.Ratio_Full -> R.drawable.ic_full
+            }
+        )
     }
 }
 
